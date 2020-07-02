44d53666ccc63c5d370364ec98bb58f167a64bed
Hochan Lee <hochan@utexas.edu>
2020-07-02 09:14:17 +0000

    Replace URL starting from digits

--- script/makeDataReadable.sh
+++ script/makeDataReadable.sh
@@ -16,19 +16,21 @@ T_UNICODES=( 'FFFA' 'FFFB' 'FFFC'
 DIR=$1
 
 SED_PARAM=""
+# UNICODE REPLACEMENT
 for ucode in ${T_UNICODES[@]}
 do
   echo $ucode
   # Literal unicode or encoded string as UTF8.
   SED_PARAM+="\\\u${ucode}|$(echo -ne '\u'${ucode})|"
-  #SED_PARAM+="\\\u${ucode}\|"
 done
-
 echo "Ucode construction done .."
-
 SED_PARAM=${SED_PARAM::-1}
 
-SED_PARAM="s/${SED_PARAM}//g"
+# URL STARTING WITH DIGIT REPLACEMENT
+SED_PARAM="s/${SED_PARAM}//g;"
+SED_PARAM=${SED_PARAM}"s/<http:\\/\\/[0-9]*[^>]+>/<http:\\/\\/replaced.com>/g;"
+SED_PARAM=${SED_PARAM}"s/<https:\\/\\/[0-9]*[^>]+>/<https:\\/\\/replaced.com>/g"
+
 echo $SED_PARAM
 
 echo "Target DIR:" $1
