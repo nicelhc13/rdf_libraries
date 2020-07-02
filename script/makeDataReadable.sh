cbdbecb6102f2c46432b4bf1694ffbab1e0745b1
Hochan Lee <hochan@utexas.edu>
2020-07-02 20:05:30 +0000

    Check if the URL starts from digits

--- script/makeDataReadable.sh
+++ script/makeDataReadable.sh
@@ -28,15 +28,15 @@ SED_PARAM=${SED_PARAM::-1}
 
 # URL STARTING WITH DIGIT REPLACEMENT
 SED_PARAM="s/${SED_PARAM}//g;"
-SED_PARAM=${SED_PARAM}"s/<http:\\/\\/[0-9]*[^>]+>/<http:\\/\\/replaced.com>/g;"
-SED_PARAM=${SED_PARAM}"s/<https:\\/\\/[0-9]*[^>]+>/<https:\\/\\/replaced.com>/g"
+SED_PARAM=${SED_PARAM}"s/<http:\\/\\/[0-9]+[^>]+>/<http:\\/\\/replaced.com>/g;"
+SED_PARAM=${SED_PARAM}"s/<https:\\/\\/[0-9]+[^>]+>/<https:\\/\\/replaced.com>/g"
 
 echo $SED_PARAM
 
 echo "Target DIR:" $1
 
-for f in $DIR/*.ttl
-#for f in $DIR/*.nt
+#for f in $DIR/*.ttl
+for f in $DIR/*.nt
 do
   fname=${f##*/}
   echo "Processing $fname file.."
