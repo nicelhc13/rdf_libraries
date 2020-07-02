236e13b23b661156c2cc06edb4cbd22e52e12fc9
Hochan Lee <hochan@utexas.edu>
2020-07-02 08:54:19 +0000

    Remove double quote from the target

--- script/makeDataReadable.sh
+++ script/makeDataReadable.sh
@@ -5,7 +5,13 @@ T_UNICODES=( 'FFFA' 'FFFB' 'FFFC'
              'FFF0' 'FFF1' 'FFF2'
              'FFF3' 'FFF4' 'FFF5'
              'FFF6' 'FFF7' 'FFF8'
-             'FFF9' )
+             'FFF9' '0027' '0025'
+             '0060' '00B4' '2018'
+             '2019' '201C' '201D' )
+
+#T_UNICODES=( '0060' )
+              # control, double quote,
+              # quote, percentage unicodes
 
 DIR=$1
 
@@ -14,22 +20,25 @@ for ucode in ${T_UNICODES[@]}
 do
   echo $ucode
   # Literal unicode or encoded string as UTF8.
-  SED_PARAM+="\\\u"${ucode}"|"$(echo -ne '\u'${ucode})"|"
-#SED_PARAM+="\\u"${ucode}"|"
+  SED_PARAM+="\\\u${ucode}|$(echo -ne '\u'${ucode})|"
+  #SED_PARAM+="\\\u${ucode}\|"
 done
+
+echo "Ucode construction done .."
 
 SED_PARAM=${SED_PARAM::-1}
 
-SED_PARAM="s/(${SED_PARAM})//g"
+SED_PARAM="s/${SED_PARAM}//g"
 echo $SED_PARAM
 
 echo "Target DIR:" $1
 
-for f in $DIR/*.nt
+for f in $DIR/*.ttl
+#for f in $DIR/*.nt
 do
   fname=${f##*/}
   echo "Processing $fname file.."
-  sed -r -i "${SED_PARAM}" $DIR$fname
-  echo "sed -r -i ${SED_PARAM} $DIR$fname"
+  sed -i -r ${SED_PARAM} $DIR$fname
+  echo "sed -i -r '${SED_PARAM}' $DIR$fname"
 done
 
