8d8cfa1a731e1ca9eb5811655f204da25184163d
Hochan Lee <hochan@utexas.edu>
2020-07-09 20:00:55 +0000

    Add more filtering conditions

--- script/makeDataReadable.sh
+++ script/makeDataReadable.sh
@@ -28,19 +28,30 @@ SED_PARAM=${SED_PARAM::-1}
 
 # URL STARTING WITH DIGIT REPLACEMENT
 SED_PARAM="s/${SED_PARAM}//g;"
-SED_PARAM=${SED_PARAM}"s/<http:\\/\\/[0-9]+[^>]+>/<http:\\/\\/replaced.com>/g;"
-SED_PARAM=${SED_PARAM}"s/<https:\\/\\/[0-9]+[^>]+>/<https:\\/\\/replaced.com>/g"
+SED_PARAM=${SED_PARAM}"/^_[^:]/d;" # remove URL starts from _, but not followed by :
+#SED_PARAM=${SED_PARAM}"/<*\"+[^>]*>/d;" # remove URL having "
+#SED_PARAM=${SED_PARAM}"/^[^(<|_|\")].*/d;"
+
+#SED_PARAM=${SED_PARAM}"/WARN  riot/d;"
+#SED_PARAM=${SED_PARAM}"/INFO  riot/d;"
+#SED_PARAM=${SED_PARAM}"/<*_+[^>]*>/d;" # remove URL having _ --> it removes valid URL
+SED_PARAM=${SED_PARAM}"s/<http:\\/\\/[0-9]+[^>]*>/<http:\\/\\/replaced.com>/g;"
+SED_PARAM=${SED_PARAM}"s/<https:\\/\\/[0-9]+[^>]*>/<https:\\/\\/replaced.com>/g;"
+SED_PARAM=${SED_PARAM}"s/<ftp:\\/\\/[0-9]+[^>]*>/<ftp:\\/\\/replaced.com>/g;"
+#SED_PARAM=${SED_PARAM}"s/^(_)?:.*/_:/g" # replace _ to _:
+
 
 echo $SED_PARAM
 
 echo "Target DIR:" $1
 
-#for f in $DIR/*.ttl
-for f in $DIR/*.nt
+for f in $DIR/*.ttl
+#for f in $DIR/*.nt
 do
   fname=${f##*/}
   echo "Processing $fname file.."
-  sed -i -r ${SED_PARAM} $DIR$fname
-  echo "sed -i -r '${SED_PARAM}' $DIR$fname"
+  sed -r -E -e ${SED_PARAM} $DIR$fname > ${DIR}extract_1.nt
+  #sed -i -r -E -e ${SED_PARAM} $DIR$fname
+  echo "sed -r '${SED_PARAM}' $DIR$fname"
 done
 
