6fafa52e921a1d36770fe4eb986d5688b17923a7
nicelhc13 <hochan@utexas.edu>
2020-07-23 15:51:35 -0700

    Clean converter code

--- neo4j/convertToPG.py
+++ neo4j/convertToPG.py
@@ -1,11 +1,10 @@
-"""
-Converter from an RDF serialized text to a neo4j property graph.
-(python3 supports)
-
-Parameters:
-
--i (--input): pass the input of an RDF text
-"""
+################################################################################
+# Convert an RDF serialized text to a neo4j property graph.
+#
+# Parameters:
+#
+#-i (--input): pass the input of an RDF text
+################################################################################
 import argparse
 import os
 import sys
@@ -15,7 +14,6 @@ TURTLE = 0
 NT     = 1
 RDFXML = 2
 
-# TODO n3 is not the target.
 RDFString = [ "Turtle", "N-Triples", "RDF/XML" ] 
 
 uri = "neo4j://localhost:7687"
@@ -25,20 +23,24 @@ usrPW = "1" # NOTE: You should use your own account.
             # ID: neo4j and PW: neo4j.
 
 """
-Remove all the existing data whenver the script starts.
-Note that it would be better to remove data taking too much memory manually. 
-DB is located on /var/lib/neo4j/data/databases/*.
+Remove all existing neo4j data.
+NOTE that directly remove neo4j db directory is much faster than querying.
 """
 def delete_existing_db(session):
   print("Delete existing data..")
   session.run("MATCH (resource) DETACH DELETE resource;")
-  print("Deletion done.")
+  print("Deletion done..\n")
 
+"""
+Set up db constraints and neosemantic configurations.
+"""
 def init_env(session):
   delete_existing_db(session)
-  session.run("CALL n10s.graphconfig.init({handleRDFTypes: 'LABELS_AND_NODES', handleMultival: 'ARRAY'});")
+  session.run("CALL n10s.graphconfig.init("+
+              "{handleRDFTypes: 'LABELS',"+
+              " handleMultival: 'ARRAY'});")
 
-  #session.run("DROP CONSTRAINT n10s_unique_uri")
+  # Check if unique uri constraint is already enabled.  
   uniqConstExt = False
   for constraint in session.run("call db.constraints()"):
     if (constraint[0] == "n10s_unique_uri"):
@@ -47,75 +49,51 @@ def init_env(session):
   if uniqConstExt:
     print("Unique URI constraint is already enabled.")
   else:
-    print("Enable an unique URI constraint.")
-    session.run("CREATE CONSTRAINT n10s_unique_uri ON (r:Resource) ASSERT r.uri IS UNIQUE")
+    print("Enable an unique URI constraint..")
+    session.run("CREATE CONSTRAINT n10s_unique_uri ON (r:Resource)"+
+                "ASSERT r.uri IS UNIQUE")
 
 """
-Import a specified RDF input to the neo4j PG format.
-TODO should allow users to specify configurations of the construction.
-     For example, representing multi-valued properties as an array.
-TODO get and print the returned exception statements from the CALL.
-     The current action is to happen nothing. (Should check the browser inter
-     -face in this case.)
+Import a specified RDF input to neo4j.
+
+TODO should allow users to specify configurations.
+     For example, users would can store multi-valued properties as
+     an array or overwrite the latest value.
 """
 def import_RDF_to_PG(session, input, rdfFormatIdx):
-  print("Import the input RDF.. :"+input)
-  #session.run("CALL n10s.rdf.import.fetch('file:///"+input+"', '"+RDFString[rdfFormatIdx]+"')")
-  importCypher = "CALL n10s.rdf.import.fetch('file:///"+input+"', '"+RDFString[rdfFormatIdx]+"') \
-                  YIELD terminationStatus, triplesLoaded, triplesParsed, extraInfo \
-                  RETURN extraInfo"
+  print("Import the input RDF :"+input)
+  importCypher = "CALL n10s.rdf.import.fetch('file:///"+input+"', '"+
+                 RDFString[rdfFormatIdx]+"')"+
+                 "YIELD terminationStatus, triplesLoaded, triplesParsed, "+
+                 "extraInfo RETURN extraInfo"
   with session.begin_transaction() as tx:
     res = tx.run(importCypher)
     # printout the result of the query.
     for r in res:
-      print(r["extraInfo"]);
+      print(r["extraInfo"])
       continue;
     print("Importing done.")
+  return r["extraInfo"]
 
-def import_DIR_to_PG(session, inputDir, rdfFormatIdx):
-  session.run("CALL n10s.graphconfig.init({handleRDFTypes: 'LABELS_AND_NODES', handleMultival: 'ARRAY'});")
-  uniqConstExt = False
-  for constraint in session.run("call db.constraints()"):
-    if (constraint[0] == "n10s_unique_uri"):
-      uniqConstExt = True
-
-  if uniqConstExt:
-    print("Unique URI constraint is already enabled.")
-  else:
-    print("Enable an unique URI constraint.")
-    session.run("CREATE CONSTRAINT n10s_unique_uri ON (r:Resource) ASSERT r.uri IS UNIQUE")
-
-  for fname in os.listdir(inputDir): 
-    print(">>>>>>>"+inputDir+"/"+fname);
-    #session.run("CALL n10s.rdf.import.fetch('file:///"+input+"', '"+RDFString[rdfFormatIdx]+"')")
-    importCypher = "CALL n10s.rdf.import.fetch('file:///"+inputDir+"/"+fname+"', '"+RDFString[rdfFormatIdx]+"') "
-    importCypher +="YIELD terminationStatus, triplesLoaded, triplesParsed, extraInfo "
-    importCypher +="RETURN extraInfo"
-    print(importCypher)
-    with session.begin_transaction() as tx:
-      res = tx.run(importCypher)
-      # printout the result of the query.
-      for r in res:
-        print("\t"+r["extraInfo"]);
-        continue;
-  print("Importing done.")
-
-#def dumpNeo4j(output):
-#  print("Dump the DB..")
-#  os.system("neo4j-admin dump --database=neo4j --to="+output)
-
+"""
+Export imported PG to Cypher.
+"""
 def exportToCypher(session, output):
 	session.run("CALL apoc.export.cypher.all('"+output+".cypher')");
 
+"""  
+Export imported PG to GraphML.
+"""
 def exportToGraphML(session, output, printType):
   if (printType == 1):
-      session.run("CALL apoc.export.graphml.all('"+output+".graphml', {useTypes:true, readLabels:true})")
+      session.run("CALL apoc.export.graphml.all('"+output+".graphml',"+
+                  "{useTypes:true, readLabels:true})")
   else:
-      session.run("CALL apoc.export.graphml.all('"+output+".graphml', {readLabels:true})")
+      session.run("CALL apoc.export.graphml.all('"+output+".graphml',"+
+                  "{readLabels:true})")
 	
 """
 Get the number of nodes and the relationships.
-TODO merge the two queries! (e.g. UNION or return multi values)
 """
 def get_num_nodes_edges(session):
   nNodes = session.run("MATCH (n) RETURN count(n)")
@@ -125,6 +103,7 @@ def get_num_nodes_edges(session):
 
 def main():
   optParser = argparse.ArgumentParser()
+  # Either one file or a directory could be accepted.
   optParser.add_argument('-i', '--input', type=str,
                          help='Specify an input file name having'
                                'RDF formmated texts.',
@@ -137,7 +116,7 @@ def main():
 
   optParser.add_argument('-s', '--source', type=int,
                          help='Specify an input RDF format to be converted:'
-                              '(N3: 0, Turtle: 1, NTriple: 2, RDF/XML: 3, '
+                              '(Turtle: 0, NTriple: 1, RDF/XML:2, '
                               'default=RDF/XML)')
 
   optParser.add_argument('-t', '--type', type=int,
@@ -153,7 +132,6 @@ def main():
   args = optParser.parse_args()
   inputRDFFile = args.inputRDFFile
   inputDirectory = args.inputDirectory
-  #output = args.output
   printType = args.printType
   srcRDFFormat  = RDFXML
 
@@ -171,7 +149,6 @@ def main():
   else:
     inputDirectory = "Not specified"
     print("Input directory is not specified")
-  #output = os.path.join(currPath, output)
 
   # If the I/O RDF formats are passed by an user.
   if args.source is not None:
@@ -180,12 +157,12 @@ def main():
   print("\n** Passed arguments ***************************\n ")
   print("\tInput file name: "+inputRDFFile)
   print("\tInput Dir: "+inputDirectory)
-  #print("\tOutput file name: "+output)
   print("\tInput RDF format: "+RDFString[srcRDFFormat])
   if printType == 1:
-    print("\tPrint type to GraphML")
+    print("\tPrint types to GraphML")
   print("\n*********************************************** ")
 
+  # Construct target RDF file lists.
   fileList = []
   if inputDirectory != "Not specified":
     for rdfFile in os.listdir(inputDirectory):
@@ -204,19 +181,33 @@ def main():
       rdfFile = os.path.basename(rdfPath)
       extraInfo = import_RDF_to_PG(session, rdfPath, srcRDFFormat)
       get_num_nodes_edges(session)
-      if extraInfo != "":
+      if extraInfo != "": # extraInfo should be empty if a import succeeded.
         importSuccess = False
       convertLog[rdfFile] = extraInfo
-  #exportToCypher(session, outputCypher)
 
+  print("\n\n **** Import results **** \n")
   if importSuccess == True:
     print("Importing succeeded.")
     print("Exporting started.")
+    # Only export GraphML if all RDFs are successfully imported.
     exportToGraphML(session, "graphML/"+rdfFile+".graphML", printType)
   else:
-    for fname in fileList:
-      fname = os.path.basename(fname)
-      print(fname+":"+str(convertLog[fname]))
+    failedFileList = []
+    for fpath in fileList:
+      fname = os.path.basename(fpath)
+      if convertLog[fname] != "":
+        print(fname+":"+str(convertLog[fname]))
+        failedFileList.append(fpath)
+      else:
+        print(fname+": successfully imported")
+
+  for fpath in failedFileList:
+    if os.path.isfile(fpath):
+      fname = os.path.basename(fpath)
+      # Aggregate failed RDFs to backup/ directory.
+      shutil.copy(fpath, "backup/"+fname)
+    else:
+      print("File does not exist", fpath)
 
 if __name__ == "__main__":
   main()
